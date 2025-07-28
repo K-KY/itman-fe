0219958d90ee446958b89e9a283ccbb869472051
K-KY <kyuyoungk@naver.com>
2025-07-29 01:17:07 +0900

    Refactor : 인터페이스 분리
    - 여러 곳에서 공통적으로 쓰여서 분리함

---
+++ src/interfaces/Depart.ts
@@ -0,0 +1,10 @@
+interface Depart {
+    departSeq: number;
+    departName: string;
+    description: string;
+    del: boolean;
+    createdDate: Date;
+    updatedDate: Date;
+}
+
+export type {Depart}
---
+++ src/interfaces/Employee.ts
@@ -0,0 +1,21 @@
+import type {Depart} from "./Depart.ts";
+
+interface Employee {
+    profile: File | null,
+    empSeq: number | null,
+    empName: string,
+    empNum: string,
+    enoNumber: string,
+    empEmail: string,
+    empPhone: string,
+    departDto: Depart | null,
+    manager: Employee | null,
+    del: boolean | false,
+    position: null,
+    job: null
+    hired: null,
+    state: null;
+    empStatus: null;
+}
+
+export type {Employee};
