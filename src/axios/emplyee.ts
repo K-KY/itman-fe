271f3745bf73213bb29003541ff9e160dc8433a2
K-KY <kyuyoungk@naver.com>
2025-07-31 12:12:35 +0900

    Feat : axios API 호출 함수
    - get 애 도메인 이름 추가
    - 호출만 하고 있던거 데이터 리턴 하도록 변경

--- src/axios/emplyee.ts
+++ src/axios/emplyee.ts
@@ -1,22 +1,51 @@
 import axios from 'axios'
 import type {Employee} from "../interfaces/Employee.ts";
+import type {PageRequest} from "./depart.ts";
+import {getCount, getCountAll} from "./util.ts";
 
 const BASE_URL = 'http://localhost:8080/';
 const API_URL = BASE_URL + 'employees';
 
-function get() {
-    return axios.get(API_URL);
+const empty = {
+    totalPages : 0,
+    content : []
+};
+
+async function getEmployees(pageRequest: PageRequest): Promise<{
+    totalPages: number;
+    content: Employee[]
+}> {
+    try {
+        const response = await axios.get(API_URL, {
+            params: {page: pageRequest.page, size: pageRequest.size},
+            withCredentials: true
+        });
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        return empty;
+    }
 }
 
-function post(empInfo: Employee) {
+
+function postEmployees(empInfo: Employee) {
     const body = {
         empName: empInfo.empName,
         departDto: empInfo.departDto,
         manager: empInfo.manager,
         del: empInfo.del,
-    };
+    }
     return axios.post(API_URL, body);
 }
 
-export {get, post}
+async function count(del: boolean) {
+    return await getCount(API_URL, del);
+}
+
+async function countAll() {
+    return await getCountAll(API_URL);
+}
+
+
+export {getEmployees, postEmployees,  count, countAll};
 
