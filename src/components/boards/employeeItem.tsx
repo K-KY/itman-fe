b12f49f4401691f0910a577a5212c56a16b0ce78
K-KY <kyuyoungk@naver.com>
2025-07-31 17:59:49 +0900

    Refactor : 불필요한 부분 수정

--- src/components/boards/employeeItem.tsx
+++ src/components/boards/employeeItem.tsx
@@ -15,7 +15,7 @@ const EmployeeItem = ({item} : EmployeeItemProps) => {
                     </div>
                 </div>
             </td>
-            <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 font-mono">{item.empNum}미구현</td>
+            <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 font-mono">{item.empNum}</td>
             <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">{item.departDto?.departName}</td>
             <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
                 <div className="flex items-center gap-1">
@@ -33,7 +33,7 @@ const EmployeeItem = ({item} : EmployeeItemProps) => {
                             <rect width="20" height="16" x="2" y="4" rx="2"></rect>
                             <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
                         </svg>
-                        {item.empEmail}미구현
+                        {item.empEmail}
                     </div>
                     <div className="flex items-center gap-1 text-sm">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
@@ -43,7 +43,7 @@ const EmployeeItem = ({item} : EmployeeItemProps) => {
                             <path
                                 d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
                         </svg>
-                        {item.empPhone}미구현
+                        {item.empPhone}
                     </div>
                 </div>
             </td>
