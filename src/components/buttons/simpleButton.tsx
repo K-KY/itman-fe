77a69e22dfb2c14c73c71d7bc2103dddfabc9bfc
K-KY <kyuyoungk@naver.com>
2025-08-02 15:06:10 +0900

    Refactor : 버튼에 Href 추가
    - 이름이 적절하지 않은 것 같아서 변경
        - transitionButton.tsx -> simpleButton.tsx

--- src/components/buttons/simpleButton.tsx
+++ src/components/buttons/simpleButton.tsx
@@ -1,20 +1,21 @@
 interface TransitionButton {
     text: string;
+    href?: string;
     className?: string; // optional className prop
 }
 
 /**
  * 색상 속성 필요함
  * */
-const TransitionButton = ({ text, className = '' }: TransitionButton) => {
+const SimpleButton = ({ text, className = '', href }: TransitionButton) => {
     return (
         <a
             className={`transition-all duration-75 ease-in-out rounded-md py-2 ${className}`}
-            href="/"
+            href={href}
         >
             <div className="ml-4 mr-4">{text}</div>
         </a>
     );
 };
 
-export { TransitionButton };
+export { SimpleButton };
