571ad6f49d2294888c21745b4c28e83e2f7bf7f8
K-KY <kyuyoungk@naver.com>
2025-08-04 20:53:33 +0900

    Feat : main에 job, position 라우터 추가하고 SimpleBoards에 서비스 넣어 보냄

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import './index.css'
 import {Header} from "./components/header/header.tsx";
 import {SideBar} from "./components/sideBar/sideBar.tsx";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {AssetBoard, DepartBoard} from "./components/boards/departBoards.tsx";
+import {AssetBoard, SimpleBoards} from "./components/boards/simpleBoards.tsx";
 import EmployeeBoard from "./components/boards/employeeBoards.tsx";
 import NewEmployee from "./components/boards/newEmployee.tsx";
 import Login from "./components/login.tsx";
+import {DepartService} from "./service/DepartService.ts";
+import {JobService} from "./service/JobService.ts";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
@@ -18,9 +20,10 @@ createRoot(document.getElementById('root')!).render(
                     <SideBar />
                     <div className="flex-1 overflow-y-auto">
                         <Routes>
-                            <Route path="/departs" element={<DepartBoard />} />
+                            <Route path="/departs" element={<SimpleBoards boardName={"부서"} service={new DepartService()} />} />
                             <Route path="/employees" element={<EmployeeBoard />} />
                             <Route path="/assets" element={<AssetBoard />} />
+                            <Route path="/jobs" element={<SimpleBoards boardName={"직무"} service={new JobService()} />} />
                             <Route path="/employees/new" element={<NewEmployee />} />
                             <Route path="/login" element={<Login/>} />
                         </Routes>
