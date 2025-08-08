8cce92af20e2362af67467c5a6a837b8701cca8a
K-KY <kyuyoungk@naver.com>
2025-08-08 18:50:03 +0900

    Feat : 메인 페이지는 앱 화면과 로그인, 회원가입 화면을 라우팅 하도록 변경

--- src/main.tsx
+++ src/main.tsx
@@ -1,34 +1,20 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import {Header} from "./components/header/header.tsx";
-import {SideBar} from "./components/sideBar/sideBar.tsx";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {AssetBoard, SimpleBoards} from "./components/boards/simpleBoards.tsx";
-import EmployeeBoard from "./components/boards/employeeBoards.tsx";
-import NewEmployee from "./components/boards/newEmployee.tsx";
+import {RegisterComplete} from "./Register.tsx";
+import {App} from "./app.tsx";
 import Login from "./components/login.tsx";
-import {DepartService} from "./service/DepartService.ts";
-import {JobService} from "./service/JobService.ts";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <BrowserRouter>
-            <div className="flex flex-col h-screen">
-                <Header />
-                <div className="flex flex-1 overflow-hidden">
-                    <SideBar />
-                    <div className="flex-1 overflow-y-auto">
-                        <Routes>
-                            <Route path="/departs" element={<SimpleBoards boardName={"부서"} service={new DepartService()} />} />
-                            <Route path="/employees" element={<EmployeeBoard />} />
-                            <Route path="/assets" element={<AssetBoard />} />
-                            <Route path="/jobs" element={<SimpleBoards boardName={"직무"} service={new JobService()} />} />
-                            <Route path="/employees/new" element={<NewEmployee />} />
-                            <Route path="/login" element={<Login/>} />
-                        </Routes>
-                    </div>
-                </div>
+            <div>
+                <Routes>
+                    <Route path="/register" element={<RegisterComplete />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/*" element={<App />} />
+                </Routes>
             </div>
         </BrowserRouter>
     </StrictMode>
