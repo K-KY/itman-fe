b43f8df0a9c80ff14f27dc83d4ed7fefcc05848b
K-KY <kyuyoungk@naver.com>
2025-08-10 01:37:01 +0900

    Feat : 로그인 된 사용자에게 로그아웃 버튼을 보여주도록 변경
    - 헤더 중복되는 부분 컴포넌트로 변경
    - 내부에서만 쓰는 컴포넌트라서 익스포트 안함

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,52 @@
 import {SimpleButton} from "../buttons/simpleButton.tsx";
+import {authMe} from "../../axios/user.ts";
+import {useEffect, useState} from "react";
 
 const Header = () => {
+    const [authenticated, setAuthenticated] = useState<boolean | null>(null);
+    useEffect(() => {
+        authMe().then((result) => {
+            if (result) {
+                setAuthenticated(true);
+            } else {
+                setAuthenticated(false);
+            }
+        }).catch(() => {
+            setAuthenticated(false);
+        })
+    }, []);
+
+    if (authenticated === null) {
+        // 새로고침시 로그인 되지 않은 상태의 UI가 보이지 않게
+        return (
+            <HeaderContainer/>
+        )
+    }
+
+
+    if (authenticated) {
+        return (
+            <HeaderContainer>
+                <SimpleButton className={"bg-gray-100 hover:bg-gray-200"} text={"로그아웃"}/>
+            </HeaderContainer>
+        )
+    }
+
+    return (
+        <HeaderContainer>
+            <SimpleButton className={"bg-gray-100 hover:bg-gray-200"} text={"로그인"} href={"/login"}/>
+            <SimpleButton className={"bg-gray-100 hover:bg-gray-200"} text={"회원가입"}/>
+        </HeaderContainer>
+    )
+
+}
+
+interface HeaderContentProps {
+    children?: React.ReactNode;
+}
+
+
+const HeaderContainer =  ({ children }: HeaderContentProps) => {
     return (
         <header>
             <div className="bg-white h-14 flex items-center px-4 shadow-lg justify-between min-w-96">
@@ -12,11 +58,11 @@ const Header = () => {
                     />
                 </a>
                 <div className="flex items-center justify-center gap-4">
-                    <SimpleButton className={"bg-gray-100 hover:bg-gray-200"} text={"로그인"} href={"/login"}/>
-                    <SimpleButton className={"bg-gray-100 hover:bg-gray-200"} text={"회원가입"}/>
+                    {children}
                 </div>
             </div>
         </header>
+
     )
 }
 
