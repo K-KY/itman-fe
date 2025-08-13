d6bead6888be3ff8cffa90adfaf0f4cc1be1bff2
K-KY <kyuyoungk@naver.com>
2025-08-13 23:17:10 +0900

    Feat : count함수에 그룹 추가

--- src/axios/util.ts
+++ src/axios/util.ts
@@ -1,8 +1,12 @@
 import axios from "axios";
 
-async function getCountAll(url: string) {
+async function getCountAll(url: string, group: number|null): Promise<number> {
     try {
-        const response = await axios.get(url + "/count", {withCredentials: true});
+        const response = await axios.get(url + "/count", {
+            params: {groupSeq: group},
+            withCredentials: true
+        });
+        console.log(response);
         return response.data;
     } catch (error) {
         console.error(error);
@@ -10,9 +14,12 @@ async function getCountAll(url: string) {
     }
 }
 
-async function getCount(url: string, del: boolean) {
+async function getCount(url: string, del: boolean, group: number|null): Promise<number> {
     try {
-        const response = await axios.get(url + `/count/${del}`, {withCredentials: true},);
+        const response = await axios.get(url + `/count/${del}`, {
+                params: {groupSeq: group},
+                withCredentials: true
+            });
         return response.data;
     } catch (error) {
         console.error(error);
